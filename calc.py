8f555e949b9cc925fc642b549ea866b6a2b6d52f
gmbueno <gmbueno1802@gmail.com>
2020-03-23 14:05:32 -0300

    working, first commit

---
+++ calc.py
@@ -0,0 +1,42 @@
+from PIL import Image
+
+'''
+This script calculates how many white pixels there are in the given image
+Used to calculate percentage of a intersection once. John asked me to
+'''
+
+img = Image.open('original_area.png') # Can be many different formats.
+pix = img.load()
+size = img.size
+# print(size)  # Get the width and hight of the image for iterating over
+# print(pix[10,10])  # Get the RGBA Value of the a pixel of an image
+# pix[x,y] = value  # Set the RGBA Value of the image (tuple)
+
+'''now calculates the field total area and paints it blue'''
+
+field_area = 0
+for row in range(0,size[0]):
+    for col in range(0,size[1]):
+        pixel = pix[row,col]
+        if(pixel[0] > 230 and pixel[1] > 230 and pixel[2] > 230):
+            pix[row,col] = (0,0,255)
+            field_area += 1
+img.save('detected_area.png')  # Save the modified pixels as .png
+
+
+'''now calculates the area of the intersection paints it blue'''
+
+img = Image.open('original_intersec.png') # Can be many different formats.
+pix = img.load()
+size = img.size
+
+intersec_area = 0
+for row in range(0,size[0]):
+    for col in range(0,size[1]):
+        pixel = pix[row,col]
+        if(pixel[0] > 230 and pixel[1] > 230 and pixel[2] > 230):
+            pix[row,col] = (0,0,255)
+            intersec_area += 1
+img.save('detected_intersec.png')  # Save the modified pixels as .png
+
+print(f'Intersection of {100*intersec_area/field_area}%')
